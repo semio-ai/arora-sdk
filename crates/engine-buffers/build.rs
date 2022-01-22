efcedc15bbfd8daa7b8b98f6e0e3f8cd1bb11fa7
Braden McDorman <bmcdorman@gmail.com>
2022-01-22 04:22:06 -0800

    Work on RPC buffers

--- crates/engine-buffers/build.rs
+++ crates/engine-buffers/build.rs
@@ -2,12 +2,15 @@ extern crate cbindgen;
 
 use std::env;
 
+use cbindgen::Language;
+
 fn main() {
   let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
   cbindgen::Builder::new()
     .with_crate(crate_dir)
+    .with_language(Language::C)
     .generate()
     .expect("Unable to generate bindings")
-    .write_to_file("../../target/include/engine.h");
+    .write_to_file("../../target/include/engine_buffers.h");
 }
---
+++ libs/cpp/include/semio/engine/buffers/BufferWriter.hpp
@@ -0,0 +1,21 @@
+#ifndef _SEMIO_ENGINE_BUFFERS_BUFFERWRITER_HPP_
+#define _SEMIO_ENGINE_BUFFERS_BUFFERWRITER_HPP_
+
+namespace semio
+{
+  namespace engine
+  {
+    namespace buffers
+    {
+      class BufferWriter
+      {
+      public:
+
+      private:
+
+      };
+    }
+  }
+}
+
+#endif
