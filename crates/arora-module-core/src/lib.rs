8f8ccee1dbebc9546eb89d6136123d16acfd0668
Victor Paléologue <victor.paleologue@palaio.eu>
2022-01-31 17:18:08 +0100

    Testing module definition parsing

--- crates/arora-module-core/src/lib.rs
+++ crates/arora-module-core/src/lib.rs
@@ -65,3 +65,57 @@ impl<'a, R: AsyncRead + Unpin> Reader<'a, R> {
     Ok(Some(value))
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use std::str::FromStr;
+  use uuid::Uuid;
+  use arora_schema::module::high::ModuleDefinition;
+
+  #[test]
+  fn parse_uuid() {
+    let uuid_string = "b41899c3-66dc-40d4-ab61-d1ccf5231c88";
+    let expected = Uuid::from_str(uuid_string).unwrap();
+    let actual: Uuid = serde_yaml::from_str(uuid_string).unwrap();
+    assert!(actual == expected);
+  }
+
+  #[test]
+  fn load_simple_module() {
+    let module_string = "id: 325c5e47-32db-4e23-a38f-7a2849647e0c
+author: Semio
+description: Test C++ module
+license: Proprietary
+name: test-cpp
+version:
+  major: 0
+  minor: 1
+  patch: 0
+executor:
+  name: wasm
+exports:
+  - type: function
+    id: 07f5740c-ba4a-45af-8ec5-bedde5737e99
+    name: test
+    parameters:
+      - id: b41899c3-66dc-40d4-ab61-d1ccf5231c88
+        name: a
+        type:
+          kind: scalar
+          id: Status
+      - id: 63086e48-804f-403a-8862-3358ddedc08d
+        name: b
+        type:
+          kind: scalar
+          id: s32
+    ret:
+      kind: scalar
+      id: s32
+imports: []
+dependencies: []
+executable_mime: application/wasm";
+
+    let header: ModuleDefinition = serde_yaml::from_str(module_string).unwrap();
+    assert!(header.name == "test-cpp");
+  }
+}
