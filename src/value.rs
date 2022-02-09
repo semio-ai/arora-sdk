0fc685ef1763ab603c45187f4fa7207eab56d01b
Victor Paléologue <victor.paleologue@palaio.eu>
2022-02-09 20:10:19 +0100

    Introduction of the Caller trait
    The Engine implements it, arora-cli uses it.
    A lot of code was moved around to be shared properly.
    Also includes fixes about wasm support for uuid for wasm.

---
+++ src/value.rs
@@ -0,0 +1,103 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+// Value representation for received parameters.
+//=====================================================================
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Value {
+  #[serde(rename = "unit")]
+  Unit,
+  #[serde(rename = "book")]
+  Boolean(bool),
+  #[serde(rename = "u8")]
+  U8(u8),
+  #[serde(rename = "u16")]
+  U16(u16),
+  #[serde(rename = "u32")]
+  U32(u32),
+  #[serde(rename = "u64")]
+  U64(u64),
+  #[serde(rename = "s8")]
+  S8(i8),
+  #[serde(rename = "s16")]
+  S16(i16),
+  #[serde(rename = "s32")]
+  S32(i32),
+  #[serde(rename = "s64")]
+  S64(i64),
+  #[serde(rename = "f32")]
+  R32(f32),
+  #[serde(rename = "f64")]
+  R64(f64),
+  #[serde(rename = "str")]
+  String(String),
+  #[serde(rename = "struct")]
+  Structure(Structure),
+  #[serde(rename = "enum")]
+  Enumeration(Enumeration),
+  #[serde(rename = "bool[]")]
+  ArrayBoolean(Vec<bool>),
+  #[serde(rename = "u8[]")]
+  ArrayU8(Vec<u8>),
+  #[serde(rename = "u16[]")]
+  ArrayU16(Vec<u16>),
+  #[serde(rename = "u32[]")]
+  ArrayU32(Vec<u32>),
+  #[serde(rename = "u64[]")]
+  ArrayU64(Vec<u64>),
+  #[serde(rename = "s8[]")]
+  ArrayS8(Vec<i8>),
+  #[serde(rename = "s16[]")]
+  ArrayS16(Vec<i16>),
+  #[serde(rename = "s32[]")]
+  ArrayS32(Vec<i32>),
+  #[serde(rename = "s64[]")]
+  ArrayS64(Vec<i64>),
+  #[serde(rename = "f32[]")]
+  ArrayR32(Vec<f32>),
+  #[serde(rename = "f64[]")]
+  ArrayR64(Vec<f64>),
+  #[serde(rename = "str[]")]
+  ArrayString(Vec<String>),
+  #[serde(rename = "struct[]")]
+  ArrayStructure {
+    id: Uuid,
+    elements: Vec<StructureWithoutId>,
+  },
+  #[serde(rename = "enum[]")]
+  ArrayEnumeration {
+    id: Uuid,
+    elements: Vec<EnumerationWithoutId>,
+  },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Enumeration {
+  pub id: Uuid,
+  pub variant_id: Uuid,
+  pub value: Box<Value>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Structure {
+  pub id: Uuid,
+  pub fields: Vec<StructureField>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct StructureField {
+  pub id: Uuid,
+  pub value: Box<Value>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct StructureWithoutId {
+  // #[serde(flatten)]
+  pub fields: Vec<StructureField>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct EnumerationWithoutId {
+  pub variant_id: Uuid,
+  pub value: Box<Value>,
+}
