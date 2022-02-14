a3174aa52cd76b88f76994423b3183f3c272e733
Victor Paléologue <victor.paleologue@palaio.eu>
2022-02-14 18:11:25 +0100

    Fix typo in call test yaml

--- crates/arora/src/call.rs
+++ crates/arora/src/call.rs
@@ -57,7 +57,7 @@ mod tests {
 
   pub const CALL_TEST: &'static str = "\
 id: 07f5740c-ba4a-45af-8ec5-bedde5737e99
-fields:
+args:
 - id: b41899c3-66dc-40d4-ab61-d1ccf5231c88
   value:
     enum:
@@ -83,7 +83,7 @@ fields:
 
   pub const CALL_TEST_2: &'static str = "\
 id: b213a552-77ad-465a-a26d-352e8eccfd63
-fields:
+args:
 - id: 55dbec70-1c3a-433e-a6e6-27446b7f065e
   value:
     u32: 42
