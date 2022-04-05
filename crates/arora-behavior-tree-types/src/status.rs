72d75965e4feb0d9a3c680ac6be3fc0da5b186fa
Victor Paléologue <victor.paleologue@palaio.eu>
2022-04-05 14:09:33 +0200

    Behavior Tree Types: new crate to replace arora-status

---
+++ crates/arora-behavior-tree-types/src/status.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+
+use semio_record::{
+  enumeration::v0::{public::Public as EnumerationPublic, unfrozen::EnumerationVariant},
+  ty::{Primitive, PrimitiveKind, UnfrozenTy},
+};
+use uuid::Uuid;
+
+/// Produces the declaration of the `Status` enum,
+/// so that it can be added to a registry,
+/// with the given parent, and under the ID [`STATUS_TYPE_ID`].
+pub fn declare_status_enumeration(parent: Uuid) -> EnumerationPublic {
+  EnumerationPublic {
+    name: "Status".to_string(),
+    parent,
+    variants: HashMap::from([
+      (
+        STATUS_SUCCESS_VARIANT_ID,
+        EnumerationVariant {
+          name: "Success".to_string(),
+          ty: UnfrozenTy::Primitive(Primitive {
+            kind: PrimitiveKind::Unit,
+          }),
+        },
+      ),
+      (
+        STATUS_FAILURE_VARIANT_ID,
+        EnumerationVariant {
+          name: "Failure".to_string(),
+          ty: UnfrozenTy::Primitive(Primitive {
+            kind: PrimitiveKind::Unit,
+          }),
+        },
+      ),
+      (
+        STATUS_RUNNING_VARIANT_ID,
+        EnumerationVariant {
+          name: "Running".to_string(),
+          ty: UnfrozenTy::Primitive(Primitive {
+            kind: PrimitiveKind::Unit,
+          }),
+        },
+      ),
+    ]),
+  }
+}
+
+/// Use this ID to register the type to a registry.
+pub const STATUS_ENUMERATION_ID: Uuid = Uuid::from_bytes([
+  0x32, 0x5a, 0x57, 0x67, 0xe3, 0x44, 0x45, 0x32, 0x86, 0x0e, 0x07, 0x49, 0xbc, 0xf2, 0xe4, 0x28,
+]);
+pub const STATUS_SUCCESS_VARIANT_ID: Uuid = Uuid::from_bytes([
+  0x76, 0x6e, 0x9e, 0x9a, 0x44, 0x6d, 0x4e, 0x46, 0x83, 0xe6, 0x14, 0xb7, 0xca, 0x10, 0x11, 0x69,
+]);
+pub const STATUS_FAILURE_VARIANT_ID: Uuid = Uuid::from_bytes([
+  0x24, 0x68, 0xf4, 0x6c, 0xbb, 0x60, 0x42, 0x5c, 0x9a, 0x4d, 0x9a, 0xd3, 0x26, 0xcc, 0xc7, 0xe2,
+]);
+pub const STATUS_RUNNING_VARIANT_ID: Uuid = Uuid::from_bytes([
+  0xac, 0xd7, 0x9e, 0xc6, 0x0c, 0x44, 0x40, 0x1a, 0x82, 0xf8, 0x5d, 0xa5, 0x42, 0x2d, 0x3e, 0xec,
+]);
---
+++ crates/arora-behavior-tree-types/src/tick_id.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+use semio_record::{
+  structure::v0::{public::Public as StructurePublic, unfrozen::StructureField},
+  ty::{Primitive, PrimitiveKind, UnfrozenTy},
+};
+use uuid::Uuid;
+
+pub fn declare_tick_id_structure(parent: Uuid) -> StructurePublic {
+  StructurePublic {
+    name: "tick_id".to_string(),
+    parent,
+    fields: HashMap::from([(
+      TICK_ID_CALLABLE_ID_FIELD_RAW_ID,
+      StructureField {
+        name: "callable_id".to_string(),
+        ty: UnfrozenTy::Primitive(Primitive {
+          kind: PrimitiveKind::U64,
+        }),
+      },
+    )]),
+  }
+}
+
+/// Use this ID to register the type to a registry.
+pub const TICK_ID_ENUMERATION_ID: Uuid = Uuid::from_bytes([
+  0x6f, 0x49, 0xe6, 0x50, 0x84, 0xca, 0x48, 0x99, 0xa9, 0xbd, 0x1f, 0x3b, 0xf1, 0x7f, 0xab, 0x51,
+]);
+pub const TICK_ID_CALLABLE_ID_FIELD_RAW_ID: Uuid = Uuid::from_bytes([
+  0x23, 0x79, 0x92, 0xd2, 0x17, 0xd1, 0x45, 0x9f, 0xbc, 0xa1, 0x71, 0x85, 0xfa, 0x6a, 0x69, 0xd7,
+]);
