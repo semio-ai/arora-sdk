b8e40eaa70ae350e9133d44006c5b58c5e6ea91c
Victor Paléologue <victor.paleologue@palaio.eu>
2022-04-07 16:52:15 +0200

    Registry: `TypeDefinition` can be cloned

--- crates/arora-registry/src/lib.rs
+++ crates/arora-registry/src/lib.rs
@@ -212,7 +212,7 @@ pub type UserPublic = <User as RecordDefn>::Public;
 pub type OrganizationPublic = <Organization as RecordDefn>::Public;
 pub type FolderPublic = <Folder as RecordDefn>::Public;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TypeDefinition {
   Primitive(PrimitiveKind),
   Enumeration(EnumerationPublic),
