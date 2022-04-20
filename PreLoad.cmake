87069d6bc53d7426180d8deda1e01ef472207fe5
Victor Paléologue <victor.paleologue@palaio.eu>
2022-04-20 11:18:40 +0200

    CMake: show fetching of WASI SDK

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -17,9 +17,12 @@ else ()
 endif()
 
 include(FetchContent)
+message(STATUS "Retrieving WASI SDK ${WASI_VERSION_FULL}...")
+set(FETCHCONTENT_QUIET FALSE)
 FetchContent_Declare(wasi_sdk
   URL https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-${WASI_PLATFORM}.tar.gz
 )
 FetchContent_MakeAvailable(wasi_sdk)
 set(WASI_SDK_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/_deps/wasi_sdk-src" CACHE STRING "WASI SDK Prefix")
 set(CMAKE_TOOLCHAIN_FILE "${WASI_SDK_PREFIX}/share/cmake/wasi-sdk.cmake" CACHE STRING "Toolchain file")
+message(STATUS "WASI SDK is now available in ${WASI_SDK_PREFIX}")
