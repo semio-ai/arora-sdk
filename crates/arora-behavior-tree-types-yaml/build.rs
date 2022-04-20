276ea418130a0c69cc68f39b805f23e9b3ca25ca
Victor Paléologue <victor.paleologue@palaio.eu>
2022-04-20 11:19:49 +0200

    Behavior Tree Types YAML: trigger rebuild if YAML is modified

--- crates/arora-behavior-tree-types-yaml/build.rs
+++ crates/arora-behavior-tree-types-yaml/build.rs
@@ -9,29 +9,35 @@ pub fn main() {
   // Folders.
   let folders_dir = "records/folder/";
   create_dir_all(folders_dir).unwrap();
+  println!("cargo:rerun-if-changed={}", folders_dir);
   // behavior_tree
   let behavior_tree_folder_path = format!("{}/{}.yaml", folders_dir, BEHAVIOR_TREE_FOLDER_ID);
   let behavior_tree_folder = declare_behavior_tree_folder(ROOT_ID);
   let behavior_tree_folder_yaml = serde_yaml::to_string(&behavior_tree_folder).unwrap();
-  write(behavior_tree_folder_path, behavior_tree_folder_yaml).unwrap();
+  write(&behavior_tree_folder_path, behavior_tree_folder_yaml).unwrap();
+  println!("cargo:rerun-if-changed={}", behavior_tree_folder_path);
 
   // Enumerations
   let enumerations_dir = "records/enumeration/";
   create_dir_all(&enumerations_dir).unwrap();
+  println!("cargo:rerun-if-changed={}", enumerations_dir);
   // behavior_tree::Status
   let status_enumeration_path = format!("{}/{}.yaml", enumerations_dir, STATUS_ENUMERATION_ID);
   let status_enumeration = declare_status_enumeration(BEHAVIOR_TREE_FOLDER_ID);
   let status_enumeration_yaml = serde_yaml::to_string(&status_enumeration).unwrap();
-  write(status_enumeration_path, status_enumeration_yaml).unwrap();
+  write(&status_enumeration_path, status_enumeration_yaml).unwrap();
+  println!("cargo:rerun-if-changed={}", status_enumeration_path);
 
   // Structures.
   let structures_dir = "records/structure/";
   create_dir_all(&structures_dir).unwrap();
+  println!("cargo:rerun-if-changed={}", structures_dir);
   // behavior_tree::TickId
   let tick_id_structure_path = format!("{}/{}.yaml", structures_dir, TICK_ID_STRUCTURE_ID);
   let tick_id_structure = declare_tick_id_structure(BEHAVIOR_TREE_FOLDER_ID);
   let tick_id_structure_yaml = serde_yaml::to_string(&tick_id_structure).unwrap();
   write(&tick_id_structure_path, tick_id_structure_yaml).unwrap();
+  println!("cargo:rerun-if-changed={}", tick_id_structure_path);
 }
 
 pub const ROOT_ID: Uuid = Uuid::from_bytes([
