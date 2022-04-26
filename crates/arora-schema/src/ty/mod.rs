6132650b5a65ead86b8263a7d2c590aeaaacfff2
Victor Paléologue <victor.paleologue@palaio.eu>
2022-04-26 13:58:53 +0200

    README: update arora-schema

--- crates/arora-schema/src/ty/mod.rs
+++ crates/arora-schema/src/ty/mod.rs
@@ -22,8 +22,6 @@ lazy_static::lazy_static! {
   pub static ref F32_ID: Uuid = Uuid::parse_str("00000000-0000-0000-0000-00000000000a").unwrap();
   pub static ref F64_ID: Uuid = Uuid::parse_str("00000000-0000-0000-0000-00000000000b").unwrap();
   pub static ref STRING_ID: Uuid = Uuid::parse_str("00000000-0000-0000-0000-00000000000c").unwrap();
-  pub static ref ARRAY_ID: Uuid = Uuid::parse_str("00000000-0000-0000-0000-00000000000f").unwrap();
-  pub static ref MAP_ID: Uuid = Uuid::parse_str("00000000-0000-0000-0000-000000000010").unwrap();
 
   pub static ref PRIMITIVE_IDS: HashSet<Uuid> = {
     let mut ids = HashSet::new();
@@ -40,8 +38,6 @@ lazy_static::lazy_static! {
     ids.insert(F32_ID.clone());
     ids.insert(F64_ID.clone());
     ids.insert(STRING_ID.clone());
-    ids.insert(ARRAY_ID.clone());
-    ids.insert(MAP_ID.clone());
     ids
   };
 
