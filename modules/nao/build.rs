1361e472f7ff3abdc08551b67e69f199dcda4ff4
Victor Paléologue <victor.paleologue@palaio.eu>
2022-05-11 18:13:39 +0200

    NAO: modules builds and link
    But does not install yet.

--- modules/nao/build.rs
+++ modules/nao/build.rs
@@ -12,17 +12,17 @@ fn main() {
 
 fn configure() -> PathBuf {
     let out_dir: PathBuf = env::var_os("OUT_DIR").unwrap().into();
-    let target_dir = out_dir
+    let binary_dir = out_dir
         .parent() // crate build script build dir
         .unwrap()
         .parent() // "build" dir
         .unwrap()
         .parent() // build type dir (release / debug)
-        .unwrap()
-        .parent() // target dir (i686-...)
         .unwrap();
 
-    let source_dir = target_dir
+    let source_dir = binary_dir
+        .parent() // target dir (i686-...)
+        .unwrap()
         .parent()
         .unwrap() // "target" dir
         .parent()
@@ -33,19 +33,21 @@ fn configure() -> PathBuf {
         .join("arora-behavior-tree-types-yaml")
         .join("records");
 
-    let arora_module_cli_path = target_dir
+    let arora_module_cli_path = binary_dir
+        .parent() // target dir (i686-...)
+        .unwrap()
         .parent()
         .unwrap() // "target" dir
         .join("debug")
         .join("arora-module-cli");
 
-    let arora_buffers_include_path = source_dir.join("libs").join("cpp").join("include");
-
+    let arora_cpp_source_dir = source_dir.join("libs").join("cpp");
+    
     cmake::Config::new(".")
         .define("CMAKE_TOOLCHAIN_FILE", "mac-homebrew-i686.toolchain.cmake")
-        .define("ARORA_BINARY_DIR", target_dir)
+        .define("ARORA_BINARY_DIR", binary_dir)
         .define("ARORA_MODULE_CLI", arora_module_cli_path)
         .define("ARORA_BEHAVIOR_TREE_INCLUDE", arora_behavior_tree_dir)
-        .define("ARORA_BUFFERS_CPP_INCLUDE_DIR", arora_buffers_include_path)
+        .define("ARORA_CPP_SOURCE_DIR", arora_cpp_source_dir)
         .build()
 }
